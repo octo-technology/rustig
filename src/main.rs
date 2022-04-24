9d3e5a99a4e0bfe8c4cb27afd01242e49ad70684
Dimitri Quenault <dimitri.quenault@octo.com>
2022-04-24 15:57:24 +0200

    Command init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#![allow(unused)]
+
+use clap::{Args, Parser, Subcommand};
+use std::fs;
+use std::env;
+
+#[derive(Debug,Parser)]
+#[clap(name = "rustig")]
+#[clap(about = "A simple git-like tool for the terminal", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Init {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    match args.command {
+        Commands::Init {} => {
+            init();
+        }
+    }
+}
+
+const GIR_DIR: &str = ".rustig";
+
+fn init() -> std::io::Result<()> {
+   fs::create_dir(GIR_DIR).unwrap();
+   let current_dir = env::current_dir()?;
+   println!("Initialized empty rustig repository in {}/{}",current_dir.display(), GIR_DIR);
+   Ok(())
 }

6480b741a8ab83507b48e29a86af7e34a274c741
Antoine <antoine@tanzil.li>
2022-04-28 23:25:04 +0200

    feat: implement cat-file subcommand

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use lazy_static::lazy_static;
 use sha1_smol::Digest;
 use std::fs::File;
 use std::io::{prelude::*, BufReader, BufWriter};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::{env, fs};
 
 lazy_static! {
     static ref GIT_DIR: PathBuf = env::current_dir().unwrap().join(".rustig");
+    static ref GIT_OBJECTS_DIR: PathBuf = GIT_DIR.join("objects");
 }
 
 #[derive(Parser, Debug)]
@@ -26,9 +27,17 @@ enum Commands {
     /// Hash object
     #[clap(name = "hash-object", arg_required_else_help = true)]
     HashObject {
-        /// Stuff to add
+        /// Object to hash
         #[clap(required = true, parse(from_os_str))]
         path: PathBuf,
+    },
+
+    /// Print object to stdout
+    #[clap(name = "cat-file", arg_required_else_help = true)]
+    CatFile {
+        /// Object to print
+        #[clap(required = true)]
+        object: String,
     },
 }
 
@@ -37,7 +46,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match args.command {
         Commands::Init {} => {
-            fs::create_dir_all(&*GIT_DIR.join("objects"))?;
+            fs::create_dir_all(&*GIT_OBJECTS_DIR)?;
             println!("Initialized empty Rustig repository in {:?}", *GIT_DIR);
         }
 
@@ -48,11 +57,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             buf_reader.read_to_string(&mut contents)?;
 
             let oid: Digest = sha1_smol::Sha1::from(&contents).digest();
-            let opath: &Path = &*GIT_DIR.join(oid.to_string());
+            let opath: PathBuf = GIT_OBJECTS_DIR.join(oid.to_string());
 
             let object = File::create(opath)?;
             let mut object = BufWriter::new(object);
             object.write_all(&contents.as_bytes())?;
+        }
+
+        Commands::CatFile { object } => {
+            let file = File::open(GIT_OBJECTS_DIR.join(object))?;
+            let mut buf_reader = BufReader::new(file);
+            let mut contents = String::new();
+            buf_reader.read_to_string(&mut contents)?;
+
+            for line in contents.lines() {
+                println!("{}", line);
+            }
         }
     }
 

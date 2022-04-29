73f1d3f912186b5460d4fd737f030ccea234e7d0
dama <dama@octo.com>
2022-04-30 00:26:52 +0200

    features: add parsing and init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Command};
+use std::fs;
+
+const RUSTIG_DIR: &str = ".rustig";
+
+fn cli() -> Command<'static> {
+    Command::new("rustig")
+        .about(r#"
+        ____  _   _ ____ _____ ___ ____ 
+       |  _ \| | | / ___|_   _|_ _/ ___|
+       | |_) | | | \___ \ | |  | | |  _ 
+       |  _ <| |_| |___) || |  | | |_| |
+       |_| \_\\___/|____/ |_| |___\____|
+       
+       A minimalist Git CLI in Rust
+       "#)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("init")
+                .about("Initialize directory for rustig")
+        )
+}
+
+fn init_cmd() -> std::io::Result<()> {
+    fs::create_dir(RUSTIG_DIR)?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("init", _sub_matches)) => {
+            init_cmd();
+        }
+        _ => unreachable!(),
+    }
 }

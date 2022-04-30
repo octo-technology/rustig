eb4128c7cd885f02a7d1e06cf5852f64bdfd7a46
RobertoDuarte <roberto.duarte@octo.com>
2022-04-30 18:07:05 +0200

    chapitre 4-5 & réfacto

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,36 @@
-use std::io;
+use sha1::{Sha1, Digest};
+use std::{fs, path, error};
+use base16ct;
 
-pub fn init() -> io::Result<()> {
-    // AlreadyExists error not dealt with
-    std::fs::create_dir(".rustig")?;
+const RUSTIG_DIR: &str = ".rustig";
+const OBJECTS_DIR: &str = "objects";
+
+pub fn init() -> Result<(), Box<dyn error::Error>> {
+    fs::create_dir(RUSTIG_DIR)?;
+    fs::create_dir(path::PathBuf::from(format!("{RUSTIG_DIR}/objects")))?;
+    Ok(())
+}
+
+pub fn hash_object(path: &path::PathBuf) -> Result<(), Box<dyn error::Error>> {
+    // let mut hasher = Sha1::new();
+    // let mut file = fs::File::open(path)?;
+    // io::copy(&mut file, &mut hasher)?;
+    // let oid = base16ct::lower::encode_string(&hasher.finalize());
+    // fs::copy(path, format!("{RUSTIG_DIR}/objects/{oid}"))?;
+    // print!("{oid}");
+
+    let content = fs::read_to_string(path)?;
+    let hash = Sha1::new().chain_update(&content).finalize();
+    let oid = base16ct::lower::encode_string(&hash);
+    let output_path = path::PathBuf::from(format!("{RUSTIG_DIR}/{OBJECTS_DIR}/{oid}"));
+    fs::write(output_path, content)?;
+    println!("OID: {oid}");
+    Ok(())
+}
+
+pub fn cat_object(oid: &String) -> Result<(), Box<dyn error::Error>> {
+    let output_path = path::PathBuf::from(format!("{RUSTIG_DIR}/{OBJECTS_DIR}/{oid}"));
+    let content = fs::read_to_string(output_path)?;
+    print!("{content}");
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod data;
 
 use clap::{Parser, Subcommand};
-use data::init;
+use data::{init, hash_object, cat_object};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -12,15 +12,21 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Initialize an empty rustig repository
     Init,
+    /// Hash a file and return its OID
+    #[clap(parse(from_os_str))]
+    HashObject { path: std::path::PathBuf },
+    /// Print the content of a file by its OID
+    CatFile { oid: String },
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Init => {
-            init();
-        }
+        Commands::Init => { init() }
+        Commands::HashObject { path } => { hash_object(path) }
+        Commands::CatFile { oid } => { cat_object(oid) }
     }
 }

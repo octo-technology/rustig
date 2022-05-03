a1b1a5acd41d06ed10c495de62584abdfc3910dc
Antoine <antoine@tanzil.li>
2022-05-03 11:21:36 +0200

    refactor: move logic into dedicated files

---
+++ src/cli.rs
@@ -0,0 +1,62 @@
+use crate::data;
+use clap::{Parser, Subcommand};
+use std::io;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[clap(about = "A git implementation in Rust", long_about = None)]
+struct Cli {
+    /// The command to run
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Create an empty Rustig repository or reinitialize an existing one
+    Init,
+
+    /// Compute object ID and optionally creates a blob from a file
+    #[clap(name = "hash-object", arg_required_else_help = true)]
+    HashObject {
+        /// Object to hash
+        #[clap(required = true, parse(from_os_str))]
+        path: PathBuf,
+    },
+
+    /// Provide content or type and size information for repository objects
+    #[clap(name = "cat-file", arg_required_else_help = true)]
+    CatFile {
+        /// The name of the object to show.
+        #[clap(required = true)]
+        object: String,
+    },
+}
+
+pub fn parse() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+
+    return match args.command {
+        Commands::Init {} => Ok(init()?),
+        Commands::HashObject { path } => Ok(hash_object(path)?),
+        Commands::CatFile { object } => Ok(cat_file(object)?),
+    };
+}
+
+fn init() -> io::Result<()> {
+    let git_dir = data::init()?;
+    println!("Initialized empty Rustig repository in {}", git_dir);
+    Ok(())
+}
+
+fn hash_object(path: PathBuf) -> io::Result<()> {
+    let hash = data::hash_object(path)?;
+    println!("{}", hash);
+    Ok(())
+}
+
+fn cat_file(object: String) -> io::Result<()> {
+    let content = data::cat_file(object)?;
+    println!("{}", content);
+    Ok(())
+}

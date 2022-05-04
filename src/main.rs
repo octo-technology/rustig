5f22fecaec8f8eaaeaee94c7feda35a15169a49c
Dimitri Quenault <dimitri.quenault@octo.com>
2022-05-04 07:24:45 +0200

    Functionning hash-object and cat-file function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![allow(unused)]
 
 use clap::{Args, Parser, Subcommand};
-use std::fs;
+use sha1::{Digest, Sha1};
 use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
 
-#[derive(Debug,Parser)]
+#[derive(Debug, Parser)]
 #[clap(name = "rustig")]
 #[clap(about = "A simple git-like tool for the terminal", long_about = None)]
 struct Cli {
@@ -15,6 +18,8 @@ struct Cli {
 #[derive(Debug, Subcommand)]
 enum Commands {
     Init {},
+    HashObject { path: String },
+    CatFile { object: String },
 }
 
 fn main() {
@@ -23,14 +28,40 @@ fn main() {
         Commands::Init {} => {
             init();
         }
+        Commands::HashObject { path } => {
+            println!("{}", hash_object(path));
+        }
+        Commands::CatFile { object } => {
+            cat_file(object);
+        }
     }
 }
 
-const GIR_DIR: &str = ".rustig";
+const GIT_DIR: &str = ".rustig";
 
 fn init() -> std::io::Result<()> {
-   fs::create_dir(GIR_DIR).unwrap();
-   let current_dir = env::current_dir()?;
-   println!("Initialized empty rustig repository in {}/{}",current_dir.display(), GIR_DIR);
-   Ok(())
+    fs::create_dir(GIT_DIR).unwrap();
+    let current_dir = env::current_dir()?;
+    println!(
+        "Initialized empty rustig repository in {}/{}",
+        current_dir.display(),
+        GIT_DIR
+    );
+    fs::create_dir(format!("{}/objects", GIT_DIR)).unwrap();
+    Ok(())
+}
+
+fn hash_object(path: String) -> String {
+    let mut hasher = Sha1::new();
+    let file_content = fs::read(path).expect("Unable to read file");
+    hasher.update(&file_content);
+    let hash = format!("{:x}", hasher.finalize());
+    let mut file = File::create(format!("{}/objects/{}", GIT_DIR, hash)).expect("Unable to create file");
+    file.write_all(&file_content);
+    return hash;
+}
+
+fn cat_file(object: String) {
+    let file_content = fs::read(format!("{}/objects/{}", GIT_DIR, object)).expect("Unable to read file");
+    println!("{}", String::from_utf8_lossy(&file_content));
 }

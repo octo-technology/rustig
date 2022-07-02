9ff78e50e11fe0cc87df6f1a91c2a2d7f2135a45
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2022-07-02 20:55:16 +0200

    feat: add some tests and improve others

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,10 +1,10 @@
 use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
 use assert_fs::prelude::*;
-use predicates::prelude::predicate::{path::exists, str::is_match};
+use predicates::prelude::predicate::{eq, path::exists, str::is_match};
 use std::process::Command;
 
 #[test]
-fn subcommand_init() -> Result<(), Box<dyn std::error::Error>> {
+fn subcommand_init_ok() -> Result<(), Box<dyn std::error::Error>> {
     let cwd = assert_fs::TempDir::new()?;
 
     Command::cargo_bin("rustig")?
@@ -12,7 +12,7 @@ fn subcommand_init() -> Result<(), Box<dyn std::error::Error>> {
         .arg("init")
         .assert()
         .success()
-        .stdout(is_match("^Initialized empty Rustig repository in .*/\\.rustig\n$").unwrap());
+        .stdout(is_match("^Initialized empty Rustig repository in /.*/\\.rustig\n$").unwrap());
     cwd.child(".rustig").assert(exists());
     cwd.child(".rustig/objects").assert(exists());
 
@@ -21,7 +21,7 @@ fn subcommand_init() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn subcommand_hash_object() -> Result<(), Box<dyn std::error::Error>> {
+fn subcommand_hash_object_ok() -> Result<(), Box<dyn std::error::Error>> {
     let cwd = assert_fs::TempDir::new()?;
     let file = cwd.child("some_file.txt");
     file.write_str("Some content.\n")?;
@@ -47,7 +47,45 @@ fn subcommand_hash_object() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn subcommand_cat() -> Result<(), Box<dyn std::error::Error>> {
+fn subcommand_hash_object_uninitialized_err() -> Result<(), Box<dyn std::error::Error>> {
+    let cwd = assert_fs::TempDir::new()?;
+
+    Command::cargo_bin("rustig")?
+        .current_dir(&cwd)
+        .arg("hash-object")
+        .arg("nonexistent_file.txt")
+        .assert()
+        .code(eq(1))
+        .stderr(is_match("^fatal: not a rustig repository\n$").unwrap());
+
+    cwd.close()?;
+    Ok(())
+}
+
+#[test]
+fn subcommand_hash_object_nonexistent_file_err() -> Result<(), Box<dyn std::error::Error>> {
+    let cwd = assert_fs::TempDir::new()?;
+
+    Command::cargo_bin("rustig")?
+        .current_dir(&cwd)
+        .arg("init")
+        .assert()
+        .success();
+
+    Command::cargo_bin("rustig")?
+        .current_dir(&cwd)
+        .arg("hash-object")
+        .arg("nonexistent_file.txt")
+        .assert()
+        .code(eq(1))
+        .stderr(is_match("^fatal: could not read 'nonexistent_file.txt': No such file or directory \\(os error 2\\)\n$").unwrap());
+
+    cwd.close()?;
+    Ok(())
+}
+
+#[test]
+fn subcommand_cat_file_ok() -> Result<(), Box<dyn std::error::Error>> {
     let cwd = assert_fs::TempDir::new()?;
     let file = cwd.child("some_file.txt");
     file.write_str("Some content.\n")?;
@@ -78,6 +116,44 @@ fn subcommand_cat() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
+fn subcommand_cat_file_uninitialized_err() -> Result<(), Box<dyn std::error::Error>> {
+    let cwd = assert_fs::TempDir::new()?;
+
+    Command::cargo_bin("rustig")?
+        .current_dir(&cwd)
+        .arg("cat-file")
+        .arg("nonexistent_object")
+        .assert()
+        .code(eq(1))
+        .stderr(is_match("^fatal: not a rustig repository\n$").unwrap());
+
+    cwd.close()?;
+    Ok(())
+}
+
+#[test]
+fn subcommand_cat_file_nonexistent_object_err() -> Result<(), Box<dyn std::error::Error>> {
+    let cwd = assert_fs::TempDir::new()?;
+
+    Command::cargo_bin("rustig")?
+        .current_dir(&cwd)
+        .arg("init")
+        .assert()
+        .success();
+
+    Command::cargo_bin("rustig")?
+        .current_dir(&cwd)
+        .arg("cat-file")
+        .arg("nonexistent_object")
+        .assert()
+        .code(eq(1))
+        .stderr(is_match("^fatal: could not read object '/.*/nonexistent_object': No such file or directory \\(os error 2\\)\n$").unwrap());
+
+    cwd.close()?;
+    Ok(())
+}
+
+#[test]
 fn simple_workflow() -> Result<(), Box<dyn std::error::Error>> {
     let cwd = assert_fs::TempDir::new()?;
     let file = cwd.child("some_file.txt");
@@ -88,7 +164,7 @@ fn simple_workflow() -> Result<(), Box<dyn std::error::Error>> {
         .arg("init")
         .assert()
         .success()
-        .stdout(is_match("^Initialized empty Rustig repository in .*/\\.rustig\n$").unwrap());
+        .stdout(is_match("^Initialized empty Rustig repository in /.*/\\.rustig\n$").unwrap());
     cwd.child(".rustig").assert(exists());
     cwd.child(".rustig/objects").assert(exists());
 

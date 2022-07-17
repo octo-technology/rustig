16c6a9c81e380377674fecd99fbbf9244b715000
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2022-07-18 00:05:56 +0200

    add testcase for write-tree subcommand

--- tests/cli.rs
+++ tests/cli.rs
@@ -187,6 +187,49 @@ fn subcommand_cat_file_nonexistent_object_err() -> Result<(), Box<dyn std::error
 }
 
 #[test]
+fn subcommand_write_tree_ok() -> Result<(), Box<dyn std::error::Error>> {
+    let cwd = assert_fs::TempDir::new()?;
+    let d1 = cwd.child("some_dir");
+    let d2 = d1.child("some_nested_dir");
+    let f1 = cwd.child("some_file.txt");
+    let f2 = d1.child("some_nested_file.txt");
+    let f3 = d2.child("another_nested_file.txt");
+    d1.create_dir_all()?;
+    d2.create_dir_all()?;
+    f1.write_str("Some content.\n")?;
+    f2.write_str("Some more content.\n")?;
+    f3.write_str("Yet more content.\n")?;
+
+    Command::cargo_bin("rustig")?
+        .current_dir(&cwd)
+        .arg("init")
+        .assert()
+        .success();
+
+    Command::cargo_bin("rustig")?
+        .current_dir(&cwd)
+        .arg("write-tree")
+        .assert()
+        .success()
+        .stdout(is_match("^cf4685a94a7b854014159f4dbb128f664ef3e716\n$").unwrap());
+    cwd.child(".rustig/objects/616bde835331a5cd78401171a61a4fd54f372adb")
+        .assert(exists());
+    cwd.child(".rustig/objects/2276357f8ac1bc0b174c9ccbea7fcdbeaf2be70b")
+        .assert(exists());
+    cwd.child(".rustig/objects/a5b3c94541feba5ee9b0749d2f8ba380ec5b07f2")
+        .assert(exists());
+    cwd.child(".rustig/objects/c4487275579f0c25ff5673fc64b76a13d0adb870")
+        .assert(exists());
+    cwd.child(".rustig/objects/cf4685a94a7b854014159f4dbb128f664ef3e716")
+        .assert(exists());
+    cwd.child(".rustig/objects/d727e363541ff1b8b282bde54a780d05e8007a8f")
+        .assert(exists());
+
+    cwd.close()?;
+    Ok(())
+}
+
+#[test]
 fn simple_workflow() -> Result<(), Box<dyn std::error::Error>> {
     let cwd = assert_fs::TempDir::new()?;
     let file = cwd.child("some_file.txt");

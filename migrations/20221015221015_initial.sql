172995cae2ae0c3c424b7f517ec35831a5b371bd
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2022-10-23 15:43:21 +0200

    refactor: enforce SQL types

--- migrations/20221015221015_initial.sql
+++ migrations/20221015221015_initial.sql
@@ -2,6 +2,6 @@ CREATE TABLE "objects" (
   "id" TEXT PRIMARY KEY,
   "type" TEXT NOT NULL CHECK("type" IN ('blob','tree')),
   "data" BLOB NOT NULL
-);
+) STRICT;
 
 CREATE INDEX "objects_id" ON "objects"("id");
